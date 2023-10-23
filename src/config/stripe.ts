0a41ac06fa45ed44fdd7b14dfdeb3cc3cdbc0d77
rabbittrix <rabbittrix@hotmail.com>
2023-10-23 15:46:02 +0100

    start project

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: 'Free',
+    slug: 'free',
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: '',
+        production: '',
+      },
+    },
+  },
+  {
+    name: 'Pro',
+    slug: 'pro',
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 29,
+      priceIds: {
+        test: 'price_1O2IibDu0ZhyQ6tmci15YYP7',
+        production: '',
+      },
+    },
+  },
+]
